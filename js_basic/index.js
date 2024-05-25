198c6469b0d663717a7f494b821351fdfcf91b73
neopan00000718 <elephantneo01@gmail.com>
2024-05-26 01:39:04 +0800

    5/25 js basic 2

--- js_basic/index.js
+++ js_basic/index.js
@@ -1,30 +1,11 @@
-// let firstName = 'Neo', lastName = 'Pan';
+let name = "Alice";
+let age = 25;
 
-let name = 'Neo';
-let age = 18;
-let isMarried = false;
-let numberOfHair = undefined;
-let interest = null;
-
-// name = 1;
-// console.log(name + ' is ' + age, isMarried, numberOfHair, interest);
-
-let person = {
-    name: "Neo",
-    age: 18,
-    isMarried: false
-};
-
-let man = {
-    name: "Pan"
-}
-
-console.log(person);
-
-console.log(person.name + ' is ' + person.age);
-
-function getPerson(man) {
-    console.log(man.name + ' is ' + man.age);
-}
-
-getPerson(man);
+console.group("Person Details");
+console.log("Name:", name);
+console.log("Age:", age);
+console.groupEnd();
+// Output:
+// Person Details
+//   Name: Alice
+//   Age: 25
