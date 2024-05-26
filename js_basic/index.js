6ecea830d34e96449878f1aeefb2acfc28edb143
neopan00000718 <elephantneo01@gmail.com>
2024-05-27 02:19:56 +0800

    5/26 js basic 2

--- js_basic/index.js
+++ js_basic/index.js
@@ -1,21 +1,19 @@
-function rand() {
-    return (Math.random() * 0x7FFF);
-}
-let target = [];
-let countInI = 0;
-
-for (let i = 0; i < 5; i++) {
-    let R = rand() % 10;
-    let temparr = [], countInJ = 0;
-
-    for (let j = 0; j < R; j++)
-        temparr[countInJ++] = j;
-
-    target[countInI++] = temparr;
+// Constructor function
+function Person(name) {
+    this.name = name;
 }
 
-console.group("test");
-for (let i = 0; i < 5; i++) {
-    console.log(target[i]);
-}
-console.groupEnd();
+const person1 = new Person('Alice');
+
+
+// Adding a method to the prototype
+Person.prototype.greet = function () {
+    console.log(`Hello, my name is ${this.name}.`);
+};
+
+// Creating an object
+const person = new Person('Alice');
+
+person1.greet(); // Output: Hello, my name is Alice.
+// Accessing method from prototype
+person.greet(); // Output: Hello, my name is Alice.
