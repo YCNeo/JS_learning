e34d377f4f7f552b0a6f2afdd7e500ca27b2252a
neopan00000718 <elephantneo01@gmail.com>
2024-05-26 23:56:08 +0800

    5/26 js basic

--- js_basic/index.js
+++ js_basic/index.js
@@ -1,11 +1,21 @@
-let name = "Alice";
-let age = 25;
+function rand() {
+    return (Math.random() * 0x7FFF);
+}
+let target = [];
+let countInI = 0;
 
-console.group("Person Details");
-console.log("Name:", name);
-console.log("Age:", age);
+for (let i = 0; i < 5; i++) {
+    let R = rand() % 10;
+    let temparr = [], countInJ = 0;
+
+    for (let j = 0; j < R; j++)
+        temparr[countInJ++] = j;
+
+    target[countInI++] = temparr;
+}
+
+console.group("test");
+for (let i = 0; i < 5; i++) {
+    console.log(target[i]);
+}
 console.groupEnd();
-// Output:
-// Person Details
-//   Name: Alice
-//   Age: 25
