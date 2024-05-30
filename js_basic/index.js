c85ab1b520e10b3b1a5e13ab36c15828131e4f85
neopan00000718 <elephantneo01@gmail.com>
2024-05-30 17:40:53 +0800

    5/30 2

--- js_basic/index.js
+++ js_basic/index.js
@@ -7,8 +7,11 @@ const List = [1, -1, 0, 2, 6, 'Neo', 'Rong', true, false];
 
 // const items = filtered.map(value => value * value);
 
+let sum = 90;
+
 const items = List
     .filter(value => isValue(value))
-    .map(value => value * value);
+    .map(value => value * value)
+    .reduce((acc, value) => acc + value, sum);
 
 console.log(items);
