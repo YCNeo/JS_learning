192048603379a8c5a69ab1de324eb5d2bae729f2
neopan00000718 <elephantneo01@gmail.com>
2024-06-13 21:38:57 +0800

    React 6/13

---
+++ React_App/src/components/ListGroup.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function ListGroup() {
+  const items = ["Neo", "Rong", "Ryan", "Tim"];
+
+  // const handleClick = (event: MouseEvent) => console.log(event);
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => setSelectedIndex(index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
---
+++ React_App/src/message.tsx
@@ -0,0 +1,8 @@
+function Message(){
+    const name = 'Neo';
+    if(name)
+        return <h1>Hello {name}!</h1>;
+    return <h1>Hello world!</h1>
+}
+
+export default Message;
